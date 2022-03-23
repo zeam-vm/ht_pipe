0959d1ca8c8e919c5d00582f56ed617cd4cb5c1d
Susumu Yamazaki <zacky1972@gmail.com>
2022-03-23 09:53:51 +0900

    install and set up ex_doc

--- mix.exs
+++ mix.exs
@@ -7,7 +7,8 @@ defmodule HtPipe.MixProject do
       version: "0.1.0-dev",
       elixir: "~> 1.13",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs()
     ]
   end
 
@@ -21,7 +22,15 @@ defmodule HtPipe.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:dialyxir, "~> 1.1", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.1", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.28", only: :dev, runtime: false}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "HtPipe",
+      extras: ["README.md"]
     ]
   end
 end
