c58e53d24c47e4b617e4373e0ddc6ebd9b8d6dfc
Susumu Yamazaki <zacky1972@gmail.com>
2022-03-23 09:26:28 +0900

    remove hello

--- lib/ht_pipe.ex
+++ lib/ht_pipe.ex
@@ -2,17 +2,4 @@ defmodule HtPipe do
   @moduledoc """
   Documentation for `HtPipe`.
   """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> HtPipe.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/ht_pipe_test.exs
+++ test/ht_pipe_test.exs
@@ -1,8 +1,4 @@
 defmodule HtPipeTest do
   use ExUnit.Case
   doctest HtPipe
-
-  test "greets the world" do
-    assert HtPipe.hello() == :world
-  end
 end
