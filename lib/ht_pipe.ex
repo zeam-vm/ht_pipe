a62e341aa8668a3ba53fa461d66ed9735ba9bd99
Susumu Yamazaki <zacky1972@gmail.com>
2022-03-25 07:16:50 +0900

    implement :inner

--- lib/ht_pipe.ex
+++ lib/ht_pipe.ex
@@ -23,14 +23,6 @@ defmodule HtPipe do
     by this function
   * the caller is trapping exits
 
-  A timeout, in milliseconds or `:infinity`,
-  can be given with a default value of `5000`.
-  If the time runs out before a message from
-  the task is received, this function will return `nil`
-  and the monitor will remain active.
-  Therefore this function can be called multiple times
-  on the same task.
-
   This function assumes the task's monitor is still active
   or the monitor's `:DOWN` message is in the message queue.
   If it has been demonitored or the message already received,
@@ -44,10 +36,40 @@ defmodule HtPipe do
   `:temporary` as the `:restart` option (the default),
   as this function keeps a direct reference to the task
   which is lost if the task is restarted.
+
+  ## Options
+
+  * `:timeout` - a timeout, in milliseconds or `:infinity`,
+  can be given with a default value of `5000`.
+  If the time runs out before a message from
+  the task is received, this function will return `nil`
+  and the monitor will remain active.
+  Therefore this function can be called multiple times
+  on the same task.
+
+  * `:spawn` - the spawn strategy, may be `:inner` (the default)
+    or `:os`. `:inner` means to spawn a light-weight process
+    monitored by `Task.Supervisor`. `:os` means to spawn an
+    os-level process using `Port` and `Node`, which is robust
+    against situations where the entire Erlang VM terminates
+    abnormally, for example a NIF abort, but it reduces
+    the efficiency of parameter passing.
   """
-  @spec htp(fun(), non_neg_integer() | atom()) ::
+  @spec htp(fun(), keyword()) ::
           {:ok, any()} | {:exit | any()} | nil
-  def htp(f, timeout \\ 5000) when is_function(f) do
+  def htp(f, options \\ []) when is_function(f) do
+    timeout =
+      options
+      |> Keyword.get(:timeout, 5000)
+
+    spawn =
+      options
+      |> Keyword.get(:spawn, :inner)
+
+    htp_p(f, timeout, spawn)
+  end
+
+  defp htp_p(f, timeout, :inner) do
     task = Task.Supervisor.async_nolink(HtPipe.TaskSupervisor, f)
     Task.yield(task, timeout) || Task.shutdown(task)
   end
